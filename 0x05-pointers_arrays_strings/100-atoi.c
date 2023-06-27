ea15fe11f38ddf1daa57983827b55a57c06ab76c
engphineas <phineastheengineer@gmail.com>
2023-06-27 09:20:00 -0700

    advanced_pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _atoi - that convert a string to an integer.
+ * @s: the pointer to be converted
+ * Return: the integer to be returned
+ */
+int _atoi(char *s)
+{
+	int a = 0;
+	int num = 0;
+	int min = 1;
+	int asi = 0;
+
+	while (s[a])
+	{
+		if (s[a] == 45)
+		{
+			min *= -1;
+		}
+		while (s[a] >= 48 && s[a] <= 57)
+		{
+			asi = 1;
+			num = (num * 10) + (s[a]);
+			a++;
+		}
+		if (asi == 1)
+		{
+			break;
+		}
+		a++;
+	}
+	num *= min;
+	return (num);
+}
---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,13 @@
+#include < stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main- program that generates random valid passwords 
+ * for the program 101-crackme.
+ * Return: 0  always
+ */
+int main(void)
+{
+	char pass_word(84);
+	int index = 0, 
