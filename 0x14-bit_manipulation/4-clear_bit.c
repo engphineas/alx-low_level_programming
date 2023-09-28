14499eed4e074e92c0499b2904fbe0cde54f396f
engphineas <phineastheengineer@gmail.com>
2023-09-28 13:05:56 -0700

    bit_manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,8 +10,12 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
+	{
 		return (-1);
-
-	*n = (~(1UL << index) & *n);
-	return (1);
+	}
+	else
+	{
+		*n = (~(1UL << index) & *n);
+		return (1);
+	}
 }
